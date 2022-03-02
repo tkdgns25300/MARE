0ea5de059e8a974f81d3f0e2361eaebeb8402452
unknown <tkdgns25300@gmail.com>
2022-03-02 15:59:50 +0900

    cors 라이브러리 추가

--- server/app.js
+++ server/app.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 80;
+
+
+app.use(cors());
 
 app.use('/', (req, res) => {
     res.send('Hello WaaT!');
