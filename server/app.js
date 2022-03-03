0352af26e1f44923c90e5f85df76f8a5f94878d7
leesanghun <tkdgns25300@gmail.com>
2022-03-03 16:16:25 +0900

    추가 : route 분기(#82)

--- server/app.js
+++ server/app.js
@@ -2,17 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const connectDB = require('./db/connect');
+const userRouter = require('./routes/userRoute');
+const recipeRouter = require('./routes/recipeRoute');
 const app = express();
 const PORT = process.env.PORT || 80;
 
 
+// middleware
 app.use(cors());
+app.use(express.json());
 
+
+// routes
 app.use('/', (req, res) => {
     res.send('Hello WaaT!');
 })
-
-
+app.use('/user', userRouter);
+app.use('/recipe', recipeRouter);
 
 
 // Start Server
