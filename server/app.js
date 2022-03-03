41044a134bce4c0b09a57c0e3c72908c51f9ceb7
leesanghun <tkdgns25300@gmail.com>
2022-03-03 13:30:44 +0900

    추가 : 서버-데이터베이스 연결(#34)

--- server/app.js
+++ server/app.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const connectDB = require('./db/connect');
+const app = express();
 const PORT = process.env.PORT || 80;
 
 
@@ -11,6 +12,19 @@ app.use('/', (req, res) => {
     res.send('Hello WaaT!');
 })
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}...`);
-})
+
+
+
+// Start Server
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`Server is listening on port ${PORT}...`);
+        });
+    } catch (error) {
+        console.error(error.message);
+    }
+}
+
+start();
