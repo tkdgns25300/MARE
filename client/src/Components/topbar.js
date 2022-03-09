4926a1ec83c0ad624c8f00897c3008e17ea70c88
CHOI-K-ROCK <chl4842@gmail.com>
2022-03-09 16:20:49 +0900

    추가 : 상단 바(#58)

---
+++ client/src/Components/topbar.js
@@ -0,0 +1,49 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faArrowLeft } from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom"
+import styled from "styled-components"
+
+
+
+const Container = styled.div`
+  display: grid;
+  place-items: center;
+
+  position: relative;
+  width: 400px;
+  height: 50px;
+  background-color: beige;
+`
+
+const BackBtn = styled(FontAwesomeIcon)`
+  
+  position: absolute;
+
+  top: 0;
+  bottom: 0;
+  left: 5px;
+
+  margin: auto 0;
+  cursor : pointer;
+`
+
+const PageTitle = styled.div`
+  font-size: 1.3rem;
+`
+
+
+
+export const Topbar = ({pageTitle}) => {
+  const navigate = useNavigate()
+  
+  const handleBackBtn = () => {
+    navigate(-1)
+  }
+
+  return (
+    <Container>
+      <BackBtn icon={faArrowLeft} size="2x" onClick={handleBackBtn}/>
+      <PageTitle>{pageTitle}</PageTitle>
+    </Container>
+  )
+}
