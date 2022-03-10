4ad877fad9655cc44a87a0091360bc885af49a6e
jo-seungho <dkarlfurqkd1@gmail.com>
2022-03-10 23:30:39 +0900

    추가: 마이페이지

--- client/src/Pages/my_recipes.js
+++ client/src/Pages/my_recipes.js
@@ -1,10 +1,10 @@
-import axios from "axios"
-import { useEffect, useState } from "react"
-import styled from "styled-components"
-import { Thumbnail } from "../Components/thumbnail"
-import { Topbar } from "../Components/topbar"
+import axios from "axios";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import { Thumbnail } from "../Components/thumbnail";
+import { Topbar } from "../Components/topbar";
 
-const serverPath = process.env.REACT_APP_SERVER_PATH
+const serverPath = process.env.REACT_APP_SERVER_PATH;
 
 const Container = styled.div`
   width: 400px;
@@ -12,39 +12,35 @@ const Container = styled.div`
   place-items: center;
   min-height: 600px;
   background-color: #dfd5d2;
-`
-
+`;
 
 export const MyRecipes = ({ loginToken }) => {
-  const [recipeList, setRecipeList] = useState([])
+  const [recipeList, setRecipeList] = useState([]);
 
   const headers = {
     headers: {
-      "Authorization": `Bearer ${loginToken}`
-    }
-  }
+      Authorization: `Bearer ${loginToken}`,
+    },
+  };
   const getData = async () => {
-    await axios.get(`${serverPath}/recipe/content`, headers)
-      .then((res) => {
-        setRecipeList(res.data.data.recipe)
-      })
-  }
+    await axios.get(`${serverPath}/recipe/content`, headers).then((res) => {
+      setRecipeList(res.data.data.recipe);
+    });
+  };
 
   const Thumbnails = () => {
     if (recipeList.length > 0) {
-      return (
-        recipeList.map((el) => {
-          return <Thumbnail key={el._id} data={el} />
-        })
-      )
+      return recipeList.map((el) => {
+        return <Thumbnail key={el._id} data={el} />;
+      });
     } else {
-      return <div>등록한 레시피가 없습니다.</div>
+      return <div>등록한 레시피가 없습니다.</div>;
     }
-  }
+  };
 
   useEffect(() => {
-    getData()
-  }, [])
+    getData();
+  }, []);
 
   return (
     <div>
@@ -53,5 +49,5 @@ export const MyRecipes = ({ loginToken }) => {
         <Thumbnails />
       </Container>
     </div>
-  )
-}
+  );
+};
--- client/src/Pages/mypage.js
+++ client/src/Pages/mypage.js
@@ -5,6 +5,66 @@ import { Link, useNavigate } from "react-router-dom";
 import { Topbar } from "../Components/topbar";
 import axios from "axios";
 
-export const Mypage = () => {
-  return <div>Mypage</div>;
+const Container = styled.div``;
+
+const Slink = styled(Link)``;
+
+const Button = styled.button``;
+
+const Content = styled.span``;
+
+const Column = styled.div``;
+
+const serverPath = process.env.REACT_APP_SERVER_PATH;
+
+export const Mypage = ({ loginToken }) => {
+  const [user, setUser] = useState({});
+  const [recipe, setRecip] = useState([]);
+  const [isLogin, setIsLogin] = useState(true);
+
+  const navigate = useNavigate();
+
+  const headers = {
+    headers: {
+      Authorization: `Bearer ${loginToken}`,
+    },
+  };
+
+  const getUserInfo = async () => {
+    await axios.get(`${serverPath}/users/data`, headers).then((res) => {
+      setUser(res.data.data.userInfo);
+    });
+  };
+
+  const getData = async () => {
+    await axios.get(`${serverPath}/recipe/content`, headers).then((res) => {
+      setRecip(res.data.data.recipe);
+    });
+  };
+
+  const handleLogout = async () => {
+    await axios
+      .post(`${serverPath}/users/logout`, {}, headers)
+      .then((res) => {});
+    window.location.reload();
+  };
+
+  useEffect(() => {
+    getUserInfo();
+    getData();
+  }, []);
+
+  return (
+    <Container>
+      <Topbar pageTitle={"마이페이지"} />
+      <Column>
+        <Content>닉네임: {user.nickname}</Content>
+      </Column>
+      <Content>나의 레시피: {recipe.length}개</Content>
+      <Column>
+        <Slink to="../userinfo">내계정관리</Slink>
+      </Column>
+      <Button onClick={handleLogout}>로그아웃</Button>
+    </Container>
+  );
 };
